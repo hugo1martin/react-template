12d92db36e0dce42fd735af1a3f83496ec6d5026
Hugo MARTIN <hugomrt.94@gmail.com>
2023-09-16 17:15:23 +0200

    improve mixins style

--- src/pages/design_system.tsx
+++ src/pages/design_system.tsx
@@ -8,13 +8,35 @@ function DesignSystem() {
       <h3>Heading 3</h3>
 
       <div className="v-space-4">
+        <button className="button default right-space-2">Default</button>
         <button className="button primary right-space-2">Primary</button>
         <button className="button secondary right-space-2">Secondary</button>
-        <button className="button default right-space-2">Default</button>
-        <button className="button blank right-space-2">Default</button>
+        <button className="button primary-inverse right-space-2">
+          Primary Inverse
+        </button>
+        <button className="button secondary-inverse right-space-2">
+          Secondary Inverse
+        </button>
+        <button className="button default-inverse right-space-2">
+          Default inverse
+        </button>
+        <button className="button default-no-border right-space-2">
+          Default No Border
+        </button>
       </div>
 
       <div className="v-space-4">
+        <div className="card default bottom-space-2">
+          <h3>Card Default</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quod voluptates voluptatem quos
+            voluptatibus quae doloribus quas natus. Quisquam voluptatum,
+            quibusdam, quia, quod voluptates voluptatem quos voluptatibus quae
+            doloribus quas natus.
+          </p>
+        </div>
+
         <div className="card primary bottom-space-2">
           <h3>Card Primary</h3>
           <p>
@@ -37,8 +59,8 @@ function DesignSystem() {
           </p>
         </div>
 
-        <div className="card default bottom-space-2">
-          <h3>Card Default</h3>
+        <div className="card default-inverse bottom-space-2">
+          <h3>Card Default Inverse</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptatum, quibusdam, quia, quod voluptates voluptatem quos
@@ -48,8 +70,8 @@ function DesignSystem() {
           </p>
         </div>
 
-        <div className="card blank bottom-space-2">
-          <h3>Card Blank</h3>
+        <div className="card primary-inverse bottom-space-2">
+          <h3>Card Primary Inverse</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptatum, quibusdam, quia, quod voluptates voluptatem quos
@@ -58,6 +80,28 @@ function DesignSystem() {
             doloribus quas natus.
           </p>
         </div>
+
+        <div className="card secondary-inverse bottom-space-2">
+          <h3>Card Secondary Inverse</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quod voluptates voluptatem quos
+            voluptatibus quae doloribus quas natus. Quisquam voluptatum,
+            quibusdam, quia, quod voluptates voluptatem quos voluptatibus quae
+            doloribus quas natus.
+          </p>
+        </div>
+      </div>
+
+      <div className="card default-no-border bottom-space-2">
+        <h3>Card Default No Border</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptatum, quibusdam, quia, quod voluptates voluptatem quos
+          voluptatibus quae doloribus quas natus. Quisquam voluptatum,
+          quibusdam, quia, quod voluptates voluptatem quos voluptatibus quae
+          doloribus quas natus.
+        </p>
       </div>
 
       <input></input>
